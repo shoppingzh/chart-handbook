3768b558a17097359961e53d18d2e09b4065ad3f
shoppingzh <xpzheng6@163.com>
2022-07-25 23:35:22 +0800

    update

---
+++ docs/.vitepress/components/echarts/map/1.vue
@@ -0,0 +1,21 @@
+<template>
+  <div ref="el" class="w-full h-[400px]" />
+</template>
+
+<script setup lang="ts">
+import * as echarts from 'echarts'
+import { onMounted, ref } from 'vue'
+
+const el = ref<HTMLElement>()
+
+onMounted(() => {
+  const chart = echarts.init(el.value)
+  chart.setOption({
+    series: [{
+      type: 'map',
+      map: 'china',
+      roam: true,
+    }]
+  } as echarts.EChartsOption)
+})
+</script>
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,11 +2,15 @@ import Theme from 'vitepress/theme'
 import ui from './ui'
 import components from '../components'
 import '../styles/index.less'
+import * as echarts from 'echarts'
+import china from '../china.json'
 
 export default {
   ...Theme,
   enhanceApp({ app }) {
     app.use(ui)
     app.use(components)
+
+    echarts.registerMap('china', china)
   }
 }
